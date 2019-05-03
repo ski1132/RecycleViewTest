808573c9c05285109266701d3c0ada670411d775
Ski <rizkee2541@gmail.com>
2019-05-03 18:53:17 +0700

    first two-fragment commit

--- app/src/main/java/org/kmutnb/testapp/MainActivity.kt
+++ app/src/main/java/org/kmutnb/testapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var arrayData: ArrayList<DataModel>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -14,31 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val testFragment = TestFragment()
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment, testFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragment, testFragment).commit()
 
 
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
-
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-    }
 }
 
 

d473ef1456ffb9dd8032e7275ed0f91fd785c68f
ski <rizkee2541@gmail.com>
2019-05-08 16:11:12 +0700

    del space

--- app/src/main/java/org/kmutnb/testapp/activity/MainActivity.kt
+++ app/src/main/java/org/kmutnb/testapp/activity/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportFragmentManager.beginTransaction().replace(R.id.fragment, ShopFragment.newInstance()).commit()
     }
 
